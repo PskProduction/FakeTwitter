841bc79e142274f28b509bfbfb8e698659d75b2d
Nikita Petrov <Nikita2871@yandex.ru>
2023-12-09 15:51:45 +0300

    change models and add routes

--- app/api/endpoints/routes.py
+++ app/api/endpoints/routes.py
@@ -1,101 +1,236 @@
-from fastapi import Header, Depends, HTTPException, APIRouter
+import shutil
+
+from fastapi import Header, Depends, HTTPException, APIRouter, UploadFile
+from sqlalchemy import desc
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 
 from ...db.database import get_db
-from ...db.db_structure import User, Followers, Followings
+from ...db.db_structure import User, Subscribers, Subscriptions, Tweet, Media, Like
 
 router = APIRouter()
 
 
+# Функция получения всех твитов
 @router.get('/api/tweets')
-def get_tweets(api_key: str = Header('test')):
-    if api_key is None:
-        return {"error": "Не указан api-key"}
+def get_tweets(api_key: str = Header('test'), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.api_key == api_key).first()
 
-    tweets = []
-    return tweets
+    if user is None:
+        raise HTTPException(status_code=404, detail='Пользователь не найден')
+
+    tweets = db.query(Tweet).order_by(desc(Tweet.id)).all()
+    tweets_response = [
+        {
+            "id": tweet.id,
+            "content": tweet.text,
+            "attachments": [
+                media.image_url for media in db.query(Media)
+                .filter_by(tweet_id=tweet.id).all()
+            ],
+            "author": tweet.user,
+            "likes": tweet.likes
+        } for tweet in tweets
+    ]
+
+    return {"result": True, "tweets": tweets_response}
 
 
+# Фукция добавления нового твита
 @router.post("/api/tweets")
-async def create_tweet(api_key: str = Header('test')):
-    return {"result": True, "tweet_id": 1}
+async def create_tweet(tweet_data: dict, api_key: str = Header('test'), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.api_key == api_key).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail='Пользователь не найден')
+
+    if tweet_data not in tweet_data:
+        raise HTTPException(status_code=404, detail='Отсутствуют обязательные поля')
+
+    new_tweet = Tweet(text=tweet_data['tweet_data'], user_id=user.id)
+
+    if "tweet_media_ids" in tweet_data:
+        new_tweet.medias = tweet_data['tweet_media_ids']
+
+    db.add(new_tweet)
+    db.commit()
+    db.refresh(new_tweet)
+    return {"result": True, "tweet_id": new_tweet.id}
 
 
 @router.post("/api/medias")
-async def upload_media(api_key: str = Header('test')):
-    media_id = 123
+async def upload_media(file: UploadFile, api_key: str = Header('test'), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.api_key == api_key).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+    with open(f'media/{file.filename}, "wb"') as image:
+        shutil.copyfileobj(file.file, image)
+    db.add(
+        Media(
+            image_name=file.filename,
+            user_id=user
+        )
+    )
+    db.commit()
+    media_id = db.query(Media).order_by(desc(Media.id)).first().id
     return JSONResponse(content={"result": True, "media_id": media_id})
 
 
+# Функция удаления твита по id
 @router.delete('/api/tweets/{tweet_id}')
-async def delete_tweet(api_key: str = Header('test')):
-    return {'result': True}
-
-
-@router.post('/api/tweets/{tweet_id}/likes')
-async def like_tweet(api_key: str = Header('test')):
-    return {'result': True}
-
-
-@router.delete('/api/tweets/{tweet_id}/likes')
-async def unlike_tweet(api_key: str = Header('test')):
-    return {'result': True}
-
-
-@router.post('/api/users/{user_id}/follow')
-async def follow_user(api_key: str = Header('test')):
-    return {'result': True}
-
-
-@router.delete('/api/users/{user_id}/follow')
-async def unfollow_user(api_key: str = Header('test')):
-    return {'result': True}
-
-
-@router.get('/api/users/me')
-async def user_info(api_key: str = Header('test'), db: Session = Depends(get_db)):
-    if api_key is None:
-        return {"error": "Не указан api-key"}
-
+async def delete_tweet(tweet_id: int, api_key: str = Header('test'), db: Session = Depends(get_db)):
     user = db.query(User).filter(User.api_key == api_key).first()
-    followers = db.query(Followers).filter_by(my_id=user.id).all()
-    followings = db.query(Followings).filter_by(my_id=user.id).all()
 
     if user is None:
-        return {"result": False, "message": "Пользователь не найден"}
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+
+    tweet = db.query(Tweet).filter(Tweet.id == tweet_id, Tweet.user_id == user.id).first()
+
+    if tweet is None:
+        raise HTTPException(status_code=404, detail="Твит не найден")
+
+    db.delete(tweet)
+    db.commit()
+
+    return {'result': True}
+
+
+# Функция для лайка твита
+@router.post('/api/tweets/{tweet_id}/likes')
+async def like_tweet(tweet_id: int, api_key: str = Header('test'), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.api_key == api_key).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+
+    tweet = db.query(Tweet).filter(Tweet.id == tweet_id).first()
+
+    if tweet is None:
+        raise HTTPException(status_code=404, detail="Твит не найден")
+
+    if not any(like.user_id == user.id for like in tweet.likes):
+        tweet.likes.append(Like(user_id=user.id, user_name=user.username))
+        tweet.count_likes = str(int(tweet.count_likes) + 1)
+        db.commit()
+
+    return {'result': True}
+
+
+# Функция для удаления лайка с твита
+@router.delete('/api/tweets/{tweet_id}/likes')
+async def unlike_tweet(tweet_id: int, api_key: str = Header('test'), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.api_key == api_key).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+
+    tweet = db.query(Tweet).filter(Tweet.id == tweet_id).first()
+
+    if tweet is None:
+        raise HTTPException(status_code=404, detail="Твит не найден")
+
+    if not any(like.user_id == user.id for like in tweet.likes):
+        tweet.likes = [like for like in tweet.likes if like.user_id != user.id]
+        tweet.count_likes = str(int(tweet.count_likes) - 1)
+
+        db.commit()
+
+    return {'result': True}
+
+
+# Функция для подписки на пользователя
+@router.post('/api/users/{subscriptions_id}/follow')
+async def follow_user(subscriber_id: int, api_key: str = Header(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.api_key == api_key).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+
+    subscriber = db.get(User, subscriber_id)
+    db.add(
+        Subscriptions(
+            subscriptions_id=subscriber_id,
+            name_subscriptions=subscriber.full_name,
+            my_id=user.id
+        )
+    )
+    db.add(
+        Subscribers(
+            subscribers_id=user.id,
+            name_subscribers=user.full_name,
+            name_subscriptions=subscriber.username,
+            my_id=subscriber_id
+        )
+    )
+    db.commit()
+    return {'result': True}
+
+
+# Функция для удаления подписки на пользователя
+@router.delete('/api/users/{user_id}/follow')
+async def unfollow_user(subscriptions_id: int, api_key: str = Header('test'), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.api_key == api_key).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+
+    subscriptions = db.query(Subscriptions).filter_by(subscriptions_id=subscriptions_id, my_id=user).first()
+    db.delete(subscriptions)
+
+    subscribers = db.query(Subscribers).filter_by(subscribers_id=user, my_id=subscriptions_id).first()
+    db.delete(subscribers)
+    db.commit()
+
+    return {'result': True}
+
+
+# Фукция получения информации о текущем пользователе
+@router.get('/api/users/me')
+async def user_info(api_key: str = Header('test'), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.api_key == api_key).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+
+    subscriptions = db.query(Subscriptions).filter_by(my_id=user.id).all()
+    subscribers = db.query(Subscribers).filter_by(my_id=user.id).all()
 
     user_response = {
         "id": user.id,
         "name": user.username,
-        "followers": [{"id": following.id_subscribers,
-                       "name": following.name_followers}
-                      for following in followings],
-        "following": [{"id": following.followings_id,
-                       "name": following.followings_name}
-                      for following in followers]
+        "followers": [{"id": subscriber.subscribers_id,
+                       "name": subscriber.subscribers_name}
+                      for subscriber in subscribers],
+        "following": [{"id": subscription.subscriptions_id,
+                       "name": subscription.subscriptions_name}
+                      for subscription in subscriptions]
     }
 
     return {"result": True, "user": user_response}
 
 
+# Функция получения информации о другом пользователе
 @router.get('/api/users/{user_id}')
 async def user_info_by_id(user_id: int, db: Session = Depends(get_db)) -> dict:
     another_user = db.get(User, user_id)
 
-    followers = db.query(Followers).filter_by(my_id=user_id).all()
-    followings = db.query(Followings).filter_by(my_id=user_id).all()
-    if another_user:
-        return {
-            "result": True,
-            "user": {
-                "id": another_user.id,
-                "name": another_user.username,
-                "followers": [{"id": following.id_subscribers,
-                               "name": following.name_followers}
-                              for following in followings],
-                "following": [{"id": following.followings_id,
-                               "name": following.followings_name}
-                              for following in followers],
-            }
+    if another_user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+
+    subscriptions = db.query(Subscriptions).filter_by(my_id=user_id).all()
+    subscribers = db.query(Subscribers).filter_by(my_id=user_id).all()
+
+    return {
+        "result": True,
+        "user": {
+            "id": another_user.id,
+            "name": another_user.username,
+            "followers": [{"id": subscriber.subscribers_id,
+                           "name": subscriber.subscribers_name}
+                          for subscriber in subscribers],
+            "following": [{"id": subscription.subscriptions_id,
+                           "name": subscription.subscriptions_name}
+                          for subscription in subscriptions],
         }
+    }
--- app/db/db_structure.py
+++ app/db/db_structure.py
@@ -1,6 +1,6 @@
 import secrets
 
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.dialects.postgresql import ARRAY, JSONB
 
@@ -12,9 +12,9 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     username = Column(String(50), nullable=False)
-    followers = Column(ARRAY(JSONB), default=[])
-    following = Column(ARRAY(JSONB), default=[])
-    api_key = Column(String(), default=secrets.token_urlsafe(32), unique=True)
+    subscriptions = Column(ARRAY(JSONB), default=[])
+    subscribers = Column(ARRAY(JSONB), default=[])
+    api_key = Column(String, default=secrets.token_urlsafe(32), unique=True)
 
     def __repr__(self):
         return f"Пользователь {self.username}"
@@ -28,7 +28,7 @@ class Tweet(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     text = Column(String, nullable=False)
     medias = Column(ARRAY(JSONB), default=[])
-    count_likes = Column(String, default='0')
+    count_likes = Column(Integer, default='0')
     likes = Column(ARRAY(JSONB), default=[])
 
     like_relationships = relationship('Like', back_populates='tweet', cascade='all, delete-orphan')
@@ -47,29 +47,29 @@ class Like(Base):
     tweet = relationship('Tweet', back_populates='like_relationships')
 
 
-class Followers(Base):
-    __tablename__ = 'followers'
+class Subscribers(Base):
+    __tablename__ = 'subscribers'
 
     id = Column(Integer, primary_key=True)
-    followers_id = Column(Integer)
-    name_followers = Column(String())
-    name_followings = Column(String())
+    subscribers_id = Column(Integer)
+    subscribers_name = Column(String)
+    subscriptions_name = Column(String)
     my_id = Column(Integer)
 
     def __repr__(self):
         return f"Подписчик {self.follower_id} пользователя {self.user_id}"
 
 
-class Followings(Base):
-    __tablename__ = 'followings'
+class Subscriptions(Base):
+    __tablename__ = 'subscriptions'
 
     id = Column(Integer, primary_key=True)
-    followings_id = Column(Integer)
-    followings_name = Column(String())
+    subscriptions_id = Column(Integer)
+    subscriptions_name = Column(String)
     my_id = Column(Integer)
 
     def __repr__(self):
-        return f"Пользователь {self.user_id} подписан на {self.following_id}"
+        return f"Пользователь {self.user_id} подписан на {self.subscriber_id}"
 
 
 class Media(Base):
